ae154a7d87d622166ef32f8799fa7a883ecd8328
Nara Díaz Viñolas <rdvdev2@gmail.com>
2025-01-26 00:42:41 +0100

    All basic mechanics

--- Assets/Scripts/Soapy.cs
+++ Assets/Scripts/Soapy.cs
@@ -1,18 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Soapy : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    private static readonly int Death = Animator.StringToHash("Death");
+    
+    private Rigidbody2D _rb;
+    private Animator _animator;
+    
     public float velocity;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector3(0, -velocity, 0);
+        _rb.velocity = new Vector3(0, -velocity, 0);
     }
 
     public void OnDealtDamage(float damage)
+    {
+        velocity = 0;
+        _animator.SetTrigger(Death);
+    }
+
+    public void OnDeathEnd()
     {
         Destroy(gameObject);
     }
