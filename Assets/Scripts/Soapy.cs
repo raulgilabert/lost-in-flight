f7e2e800fdc3c172511a7d6771d6b915ad6d5bfa
Raúl Gilabert Gámez <raulgilabert@proton.me>
2025-01-25 11:36:20 +0100

    Soapy existe

---
+++ Assets/Scripts/Soapy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soapy : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(0, velocity, 0);
+    }
+}
